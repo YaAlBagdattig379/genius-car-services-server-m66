d36027e7d9f2d17af6b6fa962b6ac7d8dd8c17f3
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-02-27 17:42:18 +0530

    m66 v4 load all services and create a single service API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const Object = new MongoClient('');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -14,12 +15,30 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xw2abv6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('success')
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+        await client.connect();
+        const servicesCollection = client.db("geniusCar-m66").collection('services');
+        app.get('/service', async(req,res)=>{
+          const query = {};
+          const cursor = servicesCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+        })
+        app.get('/service/:id', async(req,res)=>{
+             const id = req.params.id;
+             const query = {_id: new ObjectId(id)};
+             const service = await servicesCollection.findOne(query);
+             res.send(service);
+        })
+       
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
